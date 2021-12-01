4cf5e9816f4a98afee4c3938d582549a6293d1eb
Adam Thalhammer <adam.thalhammer@redbubble.com>
2021-12-01 16:16:47 +1100

    add day 1 solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,24 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map { Integer.parseInt(it) }
+            .windowed(2, 1)
+            .count { it[1] > it[0] }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .map { Integer.parseInt(it) }
+            .windowed(3, 1)
+            .map { it.sum() }
+            .windowed(2, 1)
+            .count { it[1] > it[0] }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
