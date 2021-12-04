a2db6b58996ce348d57f282628b1e02d2662206c
Adam Thalhammer <adam.thalhammer@redbubble.com>
2021-12-05 10:09:58 +1100

    day 3 reformat

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,22 +1,19 @@
 fun main() {
     fun part1(input: List<String>): Int {
         val parsed = input.map { it.map(Char::digitToInt) }
-        val gamma = parsed.transposed()
-            .map { nums -> nums.groupingBy { it }.eachCount() }
+        val gamma = parsed.transposed().map { nums -> nums.groupingBy { it }.eachCount() }
             .map { counts -> counts.maxByOrNull { it.value }!!.key }
         val epsilon = gamma.map { if (it == 0) 1 else 0 }
         val gammaD = gamma.joinToString("").toInt(2)
         val epsilonD = epsilon.joinToString("").toInt(2)
-        return gammaD*epsilonD
+        return gammaD * epsilonD
     }
 
     fun part2(input: List<String>): Int {
         val parsed = input.map { it.map(Char::digitToInt) }
-
-        val o2 = findRating(parsed) { zeros, ones -> if (zeros <= ones) 1 else 0 }.joinToString("").toInt(2)
-        val co2 = findRating(parsed) { zeros, ones -> if (zeros <= ones) 0 else 1 }.joinToString("").toInt(2)
-
-        return o2 * co2
+        val o2 = findRating(parsed) { zeros, ones -> if (zeros <= ones) 1 else 0 }
+        val co2 = findRating(parsed) { zeros, ones -> if (zeros <= ones) 0 else 1 }
+        return o2.joinToString("").toInt(2) * co2.joinToString("").toInt(2)
     }
 
     // test if implementation meets criteria from the description, like:
@@ -32,11 +29,11 @@ fun main() {
 fun findRating(parsed: List<List<Int>>, pos: Int = 0, pred: (Int, Int) -> Int): List<Int> {
     if (parsed.size == 1) return parsed.first()
 
-    val needle = parsed
-        .transposed()[pos]
-        .groupingBy { it }.eachCount()
+    val needle = parsed.transposed()[pos]
+        .groupingBy { it }
+        .eachCount()
         .let {
-            pred(it.getOrDefault(0,0), it.getOrDefault(1,0))
+            pred(it.getValue(0), it.getValue(1))
         }
 
     return findRating(parsed.filter { it[pos] == needle }, pos + 1, pred)
