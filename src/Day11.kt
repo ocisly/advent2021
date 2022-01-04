6e28f448a43bf0e123f956ad9034f9b0113f0e3c
Adam Thalhammer <adam.thalhammer@redbubble.com>
2022-01-04 15:13:50 +1100

    refactor day 11

--- src/Day11.kt
+++ src/Day11.kt
@@ -1,24 +1,18 @@
 fun main() {
-
     fun part1(input: List<String>): Int {
         var levels = input.map { it.map(Char::digitToInt) }
         return generateSequence {
-            val (l, n) = step(levels)
-            levels = l
-            n
+            levels = step(levels)
+            countFlashed(levels)
         }.take(100).sum()
     }
 
     fun part2(input: List<String>): Int {
         var levels = input.map { it.map(Char::digitToInt) }
+        val allFlashing = levels.sumOf { it.count() }
         return generateSequence {
-            val (l, n) = step(levels)
-            levels = l
-            if (n == l.sumOf { it.count() }) {
-                null
-            } else {
-                n
-            }
+            levels = step(levels)
+            countFlashed(levels).takeUnless { it == allFlashing }
         }.count() + 1
     }
 
@@ -32,42 +26,43 @@ fun main() {
     println(part2(input))
 }
 
-
-fun countFlashes(flashes: List<List<Boolean>>): Int {
-    return flashes.sumOf { row -> row.count { it } }
-}
-
-
-fun step(levels: List<List<Int>>): Pair<List<List<Int>>, Int>{
+fun step(levels: List<List<Int>>): List<List<Int>> {
     var newLevels = levels.map { row -> row.map { it + 1 } }
-    var newFlashes = levels.map { it.map { false } }
-    var lastSum = 0
-    var afterFlashes = newLevels.map { row -> row.map { it > 9 } }
+    var flashed = levels.map { it.map { false } }
+    var previousFlashCount: Int
     do {
-        newLevels = newLevels.mapIndexed { i, row ->
-            row.mapIndexed { j, level ->
-                var newLevel = level
-                for (dx in -1..1) {
-                    for (dy in -1..1) {
-                        val x = i + dx
-                        val y = j + dy
-                        val adj = newLevels.getOrNull(x)?.getOrNull(y)
-                        if (adj != null && adj > 9 && !newFlashes[x][y]) {
-                            newLevel++
-                        }
-                    }
-                }
-                newLevel
-            }
-        }
-        lastSum = countFlashes(newFlashes)
-        newFlashes = afterFlashes.zip(newFlashes).map { (a, n) ->
-            a.zip(n).map { it.first || it.second }
-        }
-        afterFlashes = newLevels.map { row -> row.map { it > 9 } }
-    } while (newFlashes.sumOf { row -> row.count { it } } != lastSum)
-    return Pair(
-        newLevels.map { row -> row.map { if (it > 9) 0 else it } },
-        countFlashes(newFlashes)
-    )
+        val newFlashes = findFlashes(newLevels)
+        newLevels = nextLevels(newLevels, flashed)
+        previousFlashCount = countFlashes(flashed)
+        flashed = mergeFlashes(flashed, newFlashes)
+    } while (countFlashes(flashed) != previousFlashCount)
+    return newLevels.map { row -> row.map { if (it > 9) 0 else it } }
 }
+
+private fun nextLevels(
+    levels: List<List<Int>>,
+    flashed: List<List<Boolean>>
+) = levels.mapIndexed { i, row ->
+    row.mapIndexed { j, level ->
+        (-1..1).sumOf { dx ->
+            (-1..1).count { dy ->
+                val x = i + dx
+                val y = j + dy
+                val adj = levels.getOrNull(x)?.getOrNull(y) ?: 0
+                adj > 9 && !flashed[x][y]
+            }
+        } + level
+    }
+}
+
+private fun mergeFlashes(
+    oldFlashes: List<List<Boolean>>,
+    newFlashes: List<List<Boolean>>
+) = oldFlashes.zip(newFlashes).map { (old, new) ->
+    old.zip(new).map { (a, b) -> a || b }
+}
+
+
+private fun countFlashed(levels: List<List<Int>>) = levels.sumOf { row -> row.count { it == 0 } }
+private fun countFlashes(newFlashes: List<List<Boolean>>) = newFlashes.sumOf { row -> row.count { it } }
+private fun findFlashes(levels: List<List<Int>>) = levels.map { row -> row.map { it > 9 } }
